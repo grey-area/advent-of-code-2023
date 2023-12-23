63106f1505c6f2a63d88630807e57d7b52b7dcbf
Andrew Webb <andrew@awebb.info>
2023-12-23 19:16:22 +0000

    Day 23 part 2

--- day23/part2.py
+++ day23/part2.py
@@ -1,8 +1,6 @@
 from pathlib import Path
 from dataclasses import dataclass
 from collections import defaultdict
-import sys
-sys.setrecursionlimit(10000)
 
 
 @dataclass(frozen=True)
@@ -16,7 +14,6 @@ class Point:
 
 class Map:
     directions = [Point(0, 1), Point(0, -1), Point(1, 0), Point(-1, 0)]
-    slope_dict = {">": Point(0, 1), "<": Point(0, -1), "v": Point(1, 0), "^": Point(-1, 0)}
 
     def __init__(self, filename):
         lines = Path(filename).read_text().splitlines()
@@ -26,37 +23,56 @@ class Map:
             for i, char in enumerate(line):
                 chars[Point(j, i)] = char
 
-        self.graph = defaultdict(list)
+        graph = defaultdict(dict)
         for point, char in chars.items():
-            if char in self.slope_dict:
-                self.graph[point].append(point + self.slope_dict[char])
-            elif char == ".":
+            if char != "#":
                 for direction in self.directions:
                     new_point = point + direction
                     if new_point in chars and chars[new_point] != "#":
-                        self.graph[point].append(new_point)
+                        graph[point][new_point] = 1
+
+        self.graph = self.condense_graph(graph)
 
         self.start = Point(0, 1)
         self.end = Point(j, i - 1)
 
+    @staticmethod
+    def condense_graph(graph):
+        nodes = set(graph.keys())
+
+        for node in nodes:
+            neighbours = graph[node]
+            if len(neighbours) == 2:
+                neighbour1, neighbour2 = neighbours
+                distance = neighbours[neighbour1] + neighbours[neighbour2]
+
+                graph[neighbour1][neighbour2] = distance
+                graph[neighbour2][neighbour1] = distance
+
+                del graph[node]
+                del graph[neighbour1][node]
+                del graph[neighbour2][node]
+
+        return graph
+
     # find the longest path from start to end
     def find_paths(self):
-        paths = []
-        self._find_paths(self.start, set(), paths)
-        return paths
+        lengths = []
+        self._find_paths(self.start, set(), 0, lengths)
+        return lengths
 
-    def _find_paths(self, start, path, paths):
+    def _find_paths(self, start, path, length, lengths):
         if start == self.end:
-            paths.append(path)
+            lengths.append(length)
             return
 
         for new_point in self.graph[start]:
             if new_point not in path:
-                self._find_paths(new_point, path | {new_point}, paths)
+                new_length = length + self.graph[start][new_point]
+                self._find_paths(new_point, path | {new_point}, new_length, lengths)
 
     def find_longest_path(self):
-        paths = self.find_paths()
-        lengths = [len(path) for path in paths]
+        lengths = self.find_paths()
         return max(lengths)
 
 
